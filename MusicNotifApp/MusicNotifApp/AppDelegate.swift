6db1b38bc9ca60c6b24e1bbe20b1d1c37ce1f7e2
Zaizen Kaegyoshi <ZaizenK@seas.1437.apng.wireless-pennnet.upenn.edu>
2014-09-13 11:53:04 -0400

    Working Yosemite voice recognition

--- MusicNotifApp/MusicNotifApp/AppDelegate.swift
+++ MusicNotifApp/MusicNotifApp/AppDelegate.swift
@@ -2,13 +2,13 @@
 //  AppDelegate.swift
 //  MusicNotifApp
 //
-//  Created by Zaizen Kaegyoshi on 9/12/14.
+//  Created by Zaizen Kaegyoshi on 9/13/14.
 //  Copyright (c) 2014 Zaizen Kaegyoshi. All rights reserved.
 //
 
 import Cocoa
 
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate,NSSpeechRecognizerDelegate {
     
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var theLabel: NSTextField!
@@ -20,38 +20,49 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var statusBarItem : NSStatusItem = NSStatusItem()
     var menu: NSMenu = NSMenu()
     var menuItem : NSMenuItem = NSMenuItem()
-    
+    var listen: NSSpeechRecognizer!
+    var name: String!
     override func awakeFromNib() {
         theLabel.stringValue = "You've pressed the button \n \(buttonPresses) times!"
         
         //Add statusBarItem
         statusBarItem = statusBar.statusItemWithLength(-1)
         statusBarItem.menu = menu
-        statusBarItem.title = "MusicNotif"
+        statusBarItem.title = "Music"
         
         //Add menuItem to menu
-        menuItem.title = "Clicked"
+        menuItem.title = "Setting"
         menuItem.action = Selector("setWindowVisible:")
         menuItem.keyEquivalent = ""
         menu.addItem(menuItem)
     }
     
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
-        var listen: NSSpeechRecognizer!
-        var cmds = ["Something"]
+        listen = NSSpeechRecognizer()
+        var cmds = ["sunny","something","yousef","gerry","kumail"]
+        
+        listen.commands = cmds
+        listen.listensInForegroundOnly = false
+        listen.blocksOtherRecognizers = true
+        listen.delegate = self
+        listen.startListening()
         
         self.window!.orderOut(self)
     }
     
+    func speechRecognizer(sender: NSSpeechRecognizer!, didRecognizeCommand command: AnyObject!) {
+        print("\(command as String) is talking to you \n")
+    }
+    
     @IBAction func buttonPressed(sender: NSButton) {
         buttonPresses+=1
-        theLabel.stringValue = "You've pressed the button \n \(buttonPresses) times!"
-        menuItem.title = "Clicked \(buttonPresses)"
-        statusBarItem.title = "MusicNotif"
+        theLabel.stringValue = "Enter the closest English pronounciation of your name"
+        //menuItem.title = "Clicked \(buttonPresses)"
+        listen.stopListening()
+        statusBarItem.title = "Listen"
     }
     
     func setWindowVisible(sender: AnyObject){
         self.window!.orderFront(self)
     }
 }
-
