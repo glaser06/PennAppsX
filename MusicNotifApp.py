fc9a1815a72e2a2fe1a3b6384e74c2a1a7343561
Sunny Nahar <sunnyn@cmu.edu>
2014-09-13 11:39:32 -0400

    added multithreading

--- MusicNotifApp.py
+++ MusicNotifApp.py
@@ -2,6 +2,7 @@ import rumps, os, time, config, match
 from twilio.rest import TwilioRestClient
 import gSpeechAPI as g
 from threading import Thread
+from multiprocessing import Process, Value, Lock
 
 r = g.Recognizer()
 m = g.Microphone()
@@ -10,39 +11,38 @@ auth_token = "c983fea53652eae6595517171700e871"
 client = TwilioRestClient(account_sid, auth_token)
 annoyanceCounter = 0
 
-def checkName():
+def checkName(audio, lock, v):
     print("Called checkName")
-    with m as source:
-        audio = r.listen(source)
-        t = Thread(target = loop)
-        t.setDaemon(True)
-        t.start()
     try:
         list = r.recognize(audio, True)
         for prediction in list:
-            print(prediction["text"])
-            if(match.checkArray(prediction["text"],config.name,0.5)):
-                global annoyanceCounter
-                annoyanceCounter = annoyanceCounter + 1
-                if(annoyanceCounter == 1):
+            #print(prediction["text"])
+            if(match.checkArray(prediction["text"], config.name, 0.5)):
+                #global annoyanceCounter
+                with lock:
+                    v = v + 1
+                if(v == 1):
                     rumps.notification("Pay attention!", "Someone said your name!", "Dumbass!")
-                elif(annoyanceCounter == 2):
+                elif(v == 2):
                     message = client.messages.create(body="Pay some fucking attention to the people around you!",
                     to="+18583822455",
                     from_="+17606704339")
                 elif(annoyanceCounter == 3):
                     os.system("osascript -e 'set Volume 0'")
-                break
             print(prediction["text"])
     except LookupError:
         print("Nothing said!")
 
 class MusicNotifApp(rumps.App):
     def __init__(self):
-        super(MusicNotifApp, self).__init__("ListenUp!")
+        super(MusicNotifApp, self).__init__("MusicListener")
         self.menu = ["Preferences"]
+        v = Value('i', 0)
+        lock = Lock()
         while True:
-            t = Thread(target = checkName)
+            with m as source:
+                audio = r.listen(source)
+            t = Thread(target = checkName, args=(audio, lock, v))
             t.setDaemon(True)
             t.start()
 
