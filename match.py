a7e7a0734e25ca0272604f095aa48e02ebff0079
Sunny Nahar <sunnyn@cmu.edu>
2014-09-13 09:20:03 -0400

    updated match

--- match.py
+++ match.py
@@ -1,10 +1,9 @@
 import editd
 
-
+# s is input string
 def checkArray(s, target, threshold):
-    for i in range(0, len(s)):
-        t = s[i].split(" ")
-        for j in range(0, len(t)):
-            if editd.sM1(t[j], target) >= threshold:
-                return True
+    t = s.split(" ")
+    for j in range(0, len(t)):
+        if editd.sM1(t[j], target) >= threshold:
+            return True
     return False
