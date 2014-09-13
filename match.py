b819100a634f58b4538cd1acc41aa0c3b5e4e7a3
Yousuf M. Soliman <ysoliman@cmu.edu>
2014-09-13 12:52:47 -0400

    refactoring

--- match.py
+++
@@ -1,9 +0,0 @@
-import editd
-
-# s is input string
-def checkArray(s, target, threshold):
-    t = s.split(" ")
-    for j in range(0, len(t)):
-        if editd.sM1(t[j], target) >= threshold:
-            return True
-    return False
